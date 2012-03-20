0f7ae588cf37fe7194dc46746908817f834b3919
George Kappel <gkappel@magicfiling.com>
2012-03-20 12:30:03 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='deploy_helpers',
+      version='1.0',
+      description='Helper utilities for deployment',
+      author='George Kappel',
+      author_email='george.kappel@gmail.com',
+      url='',
+      scripts=['expand_evars']
+     )
